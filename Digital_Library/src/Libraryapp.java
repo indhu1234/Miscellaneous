be82d6db53681a1d7b28906dd95d219b8ffcc7ff
MR USER <MR USER@DESKTOP-9UC000P>
2019-09-19 12:52:57 +0530

    Digital Library sample

---
+++ Digital_Library/src/Libraryapp.java
@@ -0,0 +1,16 @@
+
+public class Libraryapp 
+{
+   public static void main(String ar[])
+   {
+	   double d;
+	   int i;
+	   d=100.89;
+	   i=22;
+	  // d=i;
+	   System.out.println("value of d= "+d);
+	   i= (int) d;
+	   System.out.println("Value of i : "+i);
+	   
+   }
+}
---
+++ Digital_Library/src/books.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+
+public class books
+{
+   String id;
+   String bname;
+   String author;
+   String nob;
+   Date issuedate;
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getBname() {
+	return bname;
+}
+public void setBname(String bname) {
+	this.bname = bname;
+}
+public String getAuthor() {
+	return author;
+}
+public void setAuthor(String author) {
+	this.author = author;
+}
+public String getNob() {
+	return nob;
+}
+public void setNob(String nob) {
+	this.nob = nob;
+}
+public Date getIssuedate() {
+	return issuedate;
+}
+public void setIssuedate(Date issuedate) {
+	this.issuedate = issuedate;
+}
+   
+   
+   
+}
